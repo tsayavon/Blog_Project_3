1be1b6be8482a2782facfcd0b2d160498d83c6c2
tsayavon <sayavongtiffany05@gmail.com>
2022-09-01 17:16:01 -0400

    Connected to MongoDB, Backend is running. Required Dependencies, Configs, Controls, and Listener. Models and routes required mongoose

---
+++ server/routes/categories.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+// CREATE CATEGORY
+router.post('/', async(req,res)=>{
+    const newCat = new Category(req.body)
+    try {
+        const savedCategory = await newCat.save()
+        res.status(200).json(savedCategory)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// GET CATEGORY
+router.get('/', async(req,res)=>{
+    try {
+        const categories = await Category.find()
+        res.status(200).json(categories)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router;
