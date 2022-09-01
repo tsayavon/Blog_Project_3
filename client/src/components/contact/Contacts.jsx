a3f10813e77a7845fcdd81a53565e14ee1c3bcd9
tsayavon <sayavongtiffany05@gmail.com>
2022-09-01 17:32:25 -0400

    Folders set up

---
+++ client/src/components/contact/Contacts.jsx
@@ -0,0 +1,20 @@
+import './contacts.css'
+
+export default function Contacts() {
+    return (
+        <>
+            <div className='contact'>
+                <div className="singlePostWrapper">
+                    <img src="https://www.greenscene.co.id/wp-content/uploads/2022/05/Yor-Briar.webp" alt="" className="singlePostImg" />
+                    <h1 className="singlePostTitle">
+                        Lorem, ipsum dolor sit amet.
+                    </h1>
+                    <div className="singlePostInfo">
+                        <span className='singlePostAuthor'>Assassin : <b>Yor Forger</b></span>
+                    </div>
+                    <p className='singlePostDesc'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ullam sapiente voluptas inventore, quisquam deserunt eum vero neque eveniet unde provident voluptates commodi placeat ducimus explicabo quidem. Officia, corrupti. Placeat!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ullam sapiente voluptas inventore, quisquam deserunt eum vero neque eveniet unde provident voluptates commodi placeat ducimus explicabo quidem. Officia, corrupti. Placeat!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ullam sapiente voluptas inventore, quisquam deserunt eum vero neque eveniet unde provident voluptates commodi placeat ducimus explicabo quidem. Officia, corrupti. Placeat!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ullam sapiente voluptas inventore, quisquam deserunt eum vero neque eveniet unde provident voluptates commodi placeat ducimus explicabo quidem. Officia, corrupti. Placeat!</p>
+                </div>
+            </div>
+        </>
+    )
+}
